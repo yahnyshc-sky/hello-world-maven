aa30d7cd3dd019ee62a283c6247c2407c2392c0e
Maks <yahnyshc.sky.personal@gmail.com>
2025-10-20 12:24:36 +0100

    Add Maks.

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -12,7 +12,7 @@ public class App
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Maks"));
         System.out.println(sayGoodbye());
     }
 
